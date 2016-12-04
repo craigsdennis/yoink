bf33726c6d2c2cbf5d322a43684f2c31a0133072
craigsdennis <craig@teamtreehouse.com>
2016-12-03 23:41:35 -0800

    Captures recursive idea...not sure if it works yet ;)

--- index.js
+++ index.js
@@ -13,14 +13,41 @@ const nightmare = Nightmare({
     show: true
 });
 
+function yoinkPages(domain, linksToCheck, yoinkedMap) {
+    yoinkedMap = yoinkedMap || new Map();
+    return linksToCheck.reduce((prev, next) => {
+        // Avoid ye olde infinite loop
+        if (yoinkedMap.hasKey(next)) {
+            console.log("Already yoinked", next);
+            return prev;
+        }
+        // TODO: write this using snippet
+        const page = yoinkPage(next);
+        prev.put(next, page);
+        const remainingPages = page.links
+            .filter(link => link.contains(domain))
+            .filter(link => prev.containsKey(link));
+        // Recurse
+        return yoinkPages(domain, remainingPages, prev);
+    }, yoinkedMap);
+}
+
+function yoinkPage(url) {
+
+}
+
+// TODO: Synchronize this...
 nightmare
     .goto(url)
+    // TODO: optionally make this
+    .html("index.mhtml", "MHTML")
     .evaluate(() => {
         const links = [];
         for (let a of document.querySelectorAll('a').values()) {
             links.push(a.href);
         }
         return {
+            title: document.title,
             links: links,
             text: document.body.innerText
         };
