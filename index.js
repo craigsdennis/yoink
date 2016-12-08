8713a7b34eb539d8b51b82770d586257ee077936
craigsdennis <craig@teamtreehouse.com>
2016-12-07 22:21:13 -0800

    Recursin like a sailor

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const Nightmare = require("nightmare");
 
 if (process.argv.length < 3) {
-    console.log("Usage:  npm start <URL>");
+    console.log("Usage:  npm start <url> <matching-regexp>");
     process.exit(0);
 }
-const url = process.argv[2];
+const startingUrl = process.argv[2];
+const pattern = process.argv[3];
 
 const nightmare = Nightmare({
     openDevTools: {
@@ -13,49 +14,59 @@ const nightmare = Nightmare({
     show: true
 });
 
-function yoinkPages(domain, linksToCheck, yoinkedMap) {
+const re = new RegExp(pattern);
+
+function yoinkPages(linksToCheck, yoinkedMap) {
+    console.log("yoinkPages called with", linksToCheck);
+    // Start with the root page
+    linksToCheck = linksToCheck || [startingUrl];
     yoinkedMap = yoinkedMap || new Map();
     return linksToCheck.reduce((prev, next) => {
         // Avoid ye olde infinite loop
-        if (yoinkedMap.hasKey(next)) {
+        if (yoinkedMap.has(next)) {
             console.log("Already yoinked", next);
-            return prev;
-        }
-        // TODO: write this using snippet
-        const page = yoinkPage(next);
-        prev.put(next, page);
-        const remainingPages = page.links
-            .filter(link => link.contains(domain))
-            .filter(link => prev.containsKey(link));
-        // Recurse
-        return yoinkPages(domain, remainingPages, prev);
-    }, yoinkedMap);
+            return yoinkedMap;
+         }
+        return prev.then(() => {
+            return yoinkPage(next).then(page => {
+                yoinkedMap.set(next, page);
+                const remainingPages = page.links
+                    .filter(link => re.test(link))
+                    .filter(link => !link.startsWith("mailto"))
+                    .filter(link => !yoinkedMap.has(link));
+                if (remainingPages.length > 0) {
+                    // Recurse
+                    return yoinkPages(remainingPages, yoinkedMap);
+                }
+                // URL: Page object
+                return yoinkedMap;
+            });
+        });
+    }, Promise.resolve());
 }
 
 function yoinkPage(url) {
+    return nightmare
+        .goto(url)
+        // TODO: optionally make this
+        .wait(500)
+        .evaluate(() => {
+            const links = [];
+            for (let a of document.querySelectorAll('a').values()) {
+                links.push(a.href);
+            }
+            return {
+                title: document.title,
+                links: links,
+                text: document.body.innerText
+            };
+        })
+        .catch(err => console.error("YIKES!", err));
 
 }
 
-// TODO: Synchronize this...
-nightmare
-    .goto(url)
-    // TODO: optionally make this
-    .html("index.mhtml", "MHTML")
-    .evaluate(() => {
-        const links = [];
-        for (let a of document.querySelectorAll('a').values()) {
-            links.push(a.href);
-        }
-        return {
-            title: document.title,
-            links: links,
-            text: document.body.innerText
-        };
+yoinkPages()
+    .then(dict => {
+        console.log("Got back", dict);
     })
-    .end()
-    .then(data => {
-        console.log("Got the following links:", data.links);
-        console.log("And this wad of text:");
-        console.log(data.text);
-    })
-    .catch(err => console.error("Uh oh", err));
+    .catch(err => console.error("boo hoo", err));
